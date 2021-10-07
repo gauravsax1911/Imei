ccb1a8d44800d8132a7b0df1edd963ad89979d24
Gaurav Saxena <gauravsaxena1911@gmail.com>
2021-10-07 19:39:04 +0530

    second commit

---
+++ src/main/java/com/example/imei/entity/entity.java
@@ -0,0 +1,23 @@
+package com.example.imei.entity;
+
+
+public class entity {
+	
+	
+	private int id;
+           
+	
+	public String imeinumber;
+
+	public entity(int id, String imeinumber) {
+		super();
+		this.id = id;
+		this.imeinumber = imeinumber;
+	}
+
+	public entity() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+}
+	
